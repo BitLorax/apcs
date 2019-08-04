e34ce077b0838d96728bb391b6ce2a09dddffe78
BitLorax <willjhliang@gmail.com>
2019-08-03 20:22:26 -0700

    Fixed git overlap

---
+++ src/demo/Demo.java
@@ -0,0 +1,29 @@
+package demo;
+
+/**
+ * This will be our demo. Each student should add their own method to the code
+ */
+public class Demo
+{
+    // instance variables
+    private int x;
+
+    /**
+     * Constructor for objects of class Demo
+     */
+    public Demo()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    public int fishAreFriendsNotFood()
+    {
+        return 5;
+    }
+    
+    //ADD YOUR METHOD (CAN DO ANYTHING) AND INCLUDE YOUR NAME
+    public int loop() {
+        //Will Liang
+        return loop();
+    }
+}
